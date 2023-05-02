45b07f3fa81f9a861f3c8c94082c3cda0b370d24
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-05-02 16:11:18 +0300

    Deal with form submission & get user input values

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,37 @@
+import { useState, useRef } from 'react';
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+  const nameInputRef = useRef();
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    console.log(enteredName);
+
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+
+    // NOT IDEAL, DON'T MANIPULATE THE DOM:
+    // nameInputRef.current.value = '';
+    setEnteredName('');
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          type='text'
+          id='name'
+          ref={nameInputRef}
+          value={enteredName}
+          onChange={nameInputChangeHandler}
+        />
       </div>
       <div className='form-actions'>
         <button type='submit'>Submit</button>
